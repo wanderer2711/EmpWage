8ed982d9c8a26fa13f0a4099a366338e4f3506cc
Sagar <sagar.gaigawale@noustek.com>
2021-10-07 09:42:48 +0530

    fixed UC1

--- EmpWage.java
+++ EmpWage.java
@@ -1,4 +1,6 @@
 public class EmpWage {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to Emp Wage Computation");
 
