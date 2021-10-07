0ffcf47446a1d22112fbfc287676f4b73b214480
Sagar <sagar.gaigawale@noustek.com>
2021-10-07 09:40:25 +0530

    added UC1

--- EmpWage.java
+++ EmpWage.java
@@ -1,5 +1,8 @@
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Emp Wage Computation");
+
+        int EMP_CHECK = (int) Math.floor(Math.random() * 10) % 2;
+        System.out.println(EMP_CHECK);
     }
 }
